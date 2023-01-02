71d014f09b5a7119f5f44e9339bf993146c8a3be
Marcelo <marcelo.gomj@gmail.com>
2023-01-02 16:40:13 -0300

    add: comments for plugin svg and preact support

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // allow images of themoviedb
   images : {
     domains: ['image.tmdb.org'],
   },
   
   webpack( config, { dev, isServer }) {
+    // allow nextjs to load svg files
     config.module.rules.push({
       test: /.svg$/,
       use: ['@svgr/webpack']
     })
 
+    // add Preact in production build
     if (!dev && !isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
